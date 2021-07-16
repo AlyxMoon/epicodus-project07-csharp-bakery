4d7aeb55d796f31b829c977a8dc13a7bf8c3423b
AlyxMoon <allisterkmoon@gmail.com>
2021-07-16 09:29:19 -0700

    add some tests for setting up register class

--- Bakery.Tests/Models.Tests/Register.Tests.cs
+++ Bakery.Tests/Models.Tests/Register.Tests.cs
@@ -1,10 +1,38 @@
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Bakery.Models;
+using Bakery.Models.Products;
 
 namespace Bakery.Tests
 {
   [TestClass]
   public class RegisterTests
   {
+    [TestMethod]
+    public void Constructor_InitializesWithCorrectValues_SuchAsPrice ()
+    {
+      Register register = new Register();
+
+      Assert.AreEqual(register.Products.Count, 0);
+      Assert.AreEqual(register.TotalPrice, 0);
+    }
+
+    [TestMethod]
+    public void Register_CanAddProducts_GivesCorrectLengthAndPrice ()
+    {
+      Register register = new Register();
+      Bread bread1 = new Bread();
+      Pastry pastry1 = new Pastry();
+
+      register.Products.Add(bread1);
+      register.Products.Add(pastry1);
+
+      Assert.AreEqual(register.TotalPrice, 7);
+      Assert.AreEqual(register.Products.Count, 2);
+      CollectionAssert.AreEqual(
+        register.Products,
+        new List<Product> { bread1, pastry1 }
+      );
+    }
   }
 }
