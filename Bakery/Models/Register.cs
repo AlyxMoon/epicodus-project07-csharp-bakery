abdcd27dd7736dab10074b7b4aed500893c195d2
AlyxMoon <allisterkmoon@gmail.com>
2021-07-16 10:31:50 -0700

    pass tests accounting for pastry discount

--- Bakery/Models/Register.cs
+++ Bakery/Models/Register.cs
@@ -20,13 +20,16 @@ namespace Bakery.Models
     {
       int total = TotalPrice;
       int breadCount = 0;
+      int pastryCount = 0;
 
       foreach (Product item in Products)
       {
         if (item is Bread) breadCount++;
+        if (item is Pastry) pastryCount++;
       }
 
       total -= 5 * (breadCount / 2);
+      total -= (pastryCount / 3);
 
       return total;
     }
