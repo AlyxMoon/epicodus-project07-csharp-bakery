5947fe4493d63fc5e8206347ceeae0c57f0d6f7b
AlyxMoon <allisterkmoon@gmail.com>
2021-07-16 10:25:32 -0700

    add register code to account for bread discount

--- Bakery/Models/Register.cs
+++ Bakery/Models/Register.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Collections.Generic;
 using Bakery.Models.Products;
@@ -7,10 +8,27 @@ namespace Bakery.Models
   public class Register
   {
     public List<Product> Products = new List<Product>();
-    public int TotalPrice {
-      get {
+    public int TotalPrice 
+    {
+      get 
+      {
         return Products.Aggregate(0, (sum, product) => sum + product.Price);
       }
+    }
+
+    public int GetPriceWithDiscount ()
+    {
+      int total = TotalPrice;
+      int breadCount = 0;
+
+      foreach (Product item in Products)
+      {
+        if (item is Bread) breadCount++;
+      }
+
+      total -= 5 * (breadCount / 2);
+
+      return total;
     }
   }
 }
