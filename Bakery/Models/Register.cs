63d6561fc79b5919952206b48237d81eb01644a1
AlyxMoon <allisterkmoon@gmail.com>
2021-07-16 09:29:28 -0700

    fill out code for setting up the register class

--- Bakery/Models/Register.cs
+++ Bakery/Models/Register.cs
@@ -1,7 +1,16 @@
+using System.Linq;
+using System.Collections.Generic;
+using Bakery.Models.Products;
 
 namespace Bakery.Models
 {
   public class Register
   {
+    public List<Product> Products = new List<Product>();
+    public int TotalPrice {
+      get {
+        return Products.Aggregate(0, (sum, product) => sum + product.Price);
+      }
+    }
   }
 }
